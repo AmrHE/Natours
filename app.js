0130e247ecf88e28771ed251f95d02699fa51afb
AmrHE <amr.hassan.emam@gmail.com>
2021-11-28 19:11:19 +0200

    Implemented CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -26,6 +27,15 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 //1. GLOBAL MIDDLEWARE STACK
+
+//Implement CORS
+app.use(cors());
+
+// Implement CORS for complex requests
+// Respons to options requests
+// (Options is an http method like GET, POST, PATCH, DELETE, ETC...)
+app.options('*', cors());
+
 //Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
